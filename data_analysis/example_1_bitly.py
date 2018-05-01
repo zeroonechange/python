1f1a4968d311bfc8db5977abd534981b5f0a11b2
jack <zeroonechange@gmail.com>
2018-05-01 10:58:59 +0800

    first example bitly

---
+++ data_analysis/example_1_bitly.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May  1 07:59:01 2018
+
+@author: jack
+"""
+
+import pandas as pd
+
+path='/Users/jack/Desktop/data_analysis/python/data_analysis/datasets/bitly_usagov/example.txt'
+print(open(path).readline())
