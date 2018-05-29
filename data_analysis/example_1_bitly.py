e9ba9930db57c91f795dd7d65328a078d8df5fcf
jack <zeroonechange@gmail.com>
2018-05-30 00:20:00 +0800

    sympy &  plot function

--- data_analysis/example_1_bitly.py
+++ data_analysis/example_1_bitly.py
@@ -5,8 +5,3 @@ Created on Tue May  1 07:59:01 2018
 
 @author: jack
 """
-
-import pandas as pd
-
-path='/Users/jack/Desktop/data_analysis/python/data_analysis/datasets/bitly_usagov/example.txt'
-print(open(path).readline())
